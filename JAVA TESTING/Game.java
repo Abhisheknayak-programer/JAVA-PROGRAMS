fe8acd1a5831e8232ef6275f87e6f24084b8c758
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2020-11-25 17:59:11 +0530

    Add files via upload

---
+++ JAVA TESTING/Game.java
@@ -0,0 +1,7 @@
+package game;
+
+public class Game {
+    public static void main(String[] args) {
+        System.out.println(" I am a  the main game package which holds different game packages");
+    }    
+}
---
+++ JAVA TESTING/game/gamer/Game5.java
@@ -0,0 +1,7 @@
+package game.gamer;
+
+public class Game5 {
+    public static void main(String[] args) {
+        System.out.println("I am inside the game 5");
+    }
+}
